a8d230e4d3ac7f2c06dc9fdb84f501a7e1123883
ahmadjanbein <jamesjanbein@gmail.com>
2023-11-26 06:11:12 -0800

    abo joj

--- convert_numbers.c
+++ convert_numbers.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 unsigned int convert_di(va_list args, buffer_t *output,
 		unsigned char flags, int wid, int prec, unsigned char len);
 unsigned int convert_b(va_list args, buffer_t *output,
@@ -8,7 +7,6 @@ unsigned int convert_u(va_list args, buffer_t *output,
 		unsigned char flags, int wid, int prec, unsigned char len);
 unsigned int convert_o(va_list args, buffer_t *output,
 		unsigned char flags, int wid, int prec, unsigned char len);
-
 unsigned int convert_di(va_list args, buffer_t *output,
 		unsigned char flags, int wid, int prec, unsigned char len)
 {

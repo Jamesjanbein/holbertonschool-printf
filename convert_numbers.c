9d19fa9c19a4a765c4aec1b1603cfd1b2c386bdc
ahmadjanbein <jamesjanbein@gmail.com>
2023-11-26 05:57:22 -0800

    abo joj

--- convert_numbers.c
+++ convert_numbers.c
@@ -55,9 +55,7 @@ unsigned int convert_di(va_list args, buffer_t *output,
 	if (!(d == 0 && prec == 0))
 		ret += convert_sbase(output, d, "0123456789",
 				flags, 0, prec);
-
 	ret += print_neg_width(output, ret, flags, wid);
-
 	return (ret);
 }
 

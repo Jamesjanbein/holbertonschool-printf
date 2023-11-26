86e80e8a685b48553a9e130fb946dc14a78a4326
ahmadjanbein <jamesjanbein@gmail.com>
2023-11-26 05:54:40 -0800

    abo joj

--- convert_numbers.c
+++ convert_numbers.c
@@ -22,12 +22,10 @@ unsigned int convert_di(va_list args, buffer_t *output,
 		d = va_arg(args, int);
 	if (len == SHORT)
 		d = (short)d;
-
-	/* Handle space flag */
 	if (SPACE_FLAG == 1 && d >= 0)
 		ret += _memcpy(output, &space, 1);
 
-	if (prec <= 0 && NEG_FLAG == 0) /* Handle width  */
+	if (prec <= 0 && NEG_FLAG == 0)
 	{
 		if (d == LONG_MIN)
 			count += 19;
@@ -40,11 +38,8 @@ unsigned int convert_di(va_list args, buffer_t *output,
 		count += (d < 0) ? 1 : 0;
 		count += (PLUS_FLAG == 1 && d >= 0) ? 1 : 0;
 		count += (SPACE_FLAG == 1 && d >= 0) ? 1 : 0;
-
-		/* Handle plus flag when zero flag is active */
 		if (ZERO_FLAG == 1 && PLUS_FLAG == 1 && d >= 0)
 			ret += _memcpy(output, &plus, 1);
-		/*Print negative sign when zero flag is active */
 		if (ZERO_FLAG == 1 && d < 0)
 			ret += _memcpy(output, &neg, 1);
 
@@ -52,10 +47,8 @@ unsigned int convert_di(va_list args, buffer_t *output,
 		for (wid -= count; wid > 0; wid--)
 			ret += _memcpy(output, &pad, 1);
 	}
-	/* Print negative sign when zero flag is not active */
 	if (ZERO_FLAG == 0 && d < 0)
 		ret += _memcpy(output, &neg, 1);
-	/* Handle plus flag when zero flag is not active */
 	if (ZERO_FLAG == 0 && (PLUS_FLAG == 1 && d >= 0))
 		ret += _memcpy(output, &plus, 1);
 
